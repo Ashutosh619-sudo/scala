efef002c26705ea9e8a5172c38f79149b57b048e
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-11-19 15:42:29 +0530

    update

--- practical 14/Q3/practical.scala
+++ practical 14/Q3/practical.scala
@@ -13,7 +13,7 @@ object demo {
    def main(args: Array[String]):Unit={
     
     var i = List(1,2,3,5)
-    var s = List("bkchod", "teacher")
+    var s = List("nice", "teacher")
     var obj1 = new MyClass
     obj1.show(i)
     var obj2 = new MyClass
